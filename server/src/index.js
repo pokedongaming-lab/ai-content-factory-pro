7b1f0033926325b166e3fdf484d89b5c4a795e33
andredon <andredon@github.com>
2026-03-20 17:31:38 +0800

    fix: Increase JSON payload limit to 50mb for base64 images

--- server/src/index.js
+++ server/src/index.js
@@ -18,8 +18,8 @@ app.use(cors({
   origin: ['http://localhost:5173', 'http://localhost:5174'],
   credentials: true
 }))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
 // Routes
 app.use('/api/video', videoRoutes)
